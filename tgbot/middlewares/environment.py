fb8c7d89f94851e9faff592cde3562586fb88cd1
Kostiantyn Kriuchkov <36363097+Latand@users.noreply.github.com>
2022-07-24 21:18:24 +0300

    Update environment.py

--- tgbot/middlewares/environment.py
+++ tgbot/middlewares/environment.py
@@ -4,7 +4,7 @@ from aiogram.dispatcher.middlewares import LifetimeControllerMiddleware
 class EnvironmentMiddleware(LifetimeControllerMiddleware):
     skip_patterns = ["error", "update"]
     
-    def __init__(self, **kwargs)
+    def __init__(self, **kwargs):
         super().__init__()
         self.kwargs = kwargs
     
