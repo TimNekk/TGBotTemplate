8d8b98ff56060f4c4de09bcb1af572385652cc4c
Tim <herew26@gmail.com>
2022-08-06 21:51:52 +0300

    feat: add Gino base models, startup and shutdown methods

---
+++ tgbot/models/db.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from typing import List
+
+from gino import Gino
+import sqlalchemy as sa
+from loguru import logger
+
+
+db = Gino()
+
+
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    def __str__(self):
+        model = self.__class__.__name__
+        table: sa.Table = sa.inspect(self.__class__)
+        primary_key_columns: List[sa.Column] = table.primary_key.columns
+        values = {
+            column.name: getattr(self, self._column_name_map[column.name])
+            for column in primary_key_columns
+        }
+        values_str = " ".join(f"{name}={value!r}" for name, value in values.items())
+        return f"<{model} {values_str}>"
+
+
+class TimedBaseModel(BaseModel):
+    __abstract__ = True
+
+    created_at = db.Column(db.DateTime(True), server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime(True),
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+        server_default=db.func.now(),
+    )
+
+
+async def on_startup(uri: str):
+    logger.info("Setup PostgreSQL Connection")
+    await db.set_bind(uri)
+
+
+async def on_shutdown():
+    bind = db.pop_bind()
+    if bind:
+        logger.info("Close PostgreSQL Connection")
+        await bind.close()
