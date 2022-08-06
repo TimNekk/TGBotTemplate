3a9fd47c7aac5a63524ae564839bf7a0e8b2185d
Tim <herew26@gmail.com>
2022-08-06 21:59:23 +0300

    feat: add logging middleware

---
+++ tgbot/middlewares/logging.py
@@ -0,0 +1,10 @@
+from aiogram.dispatcher.middlewares import BaseMiddleware
+from aiogram.types.base import TelegramObject
+from loguru import logger
+
+
+class LoggingMiddleware(BaseMiddleware):
+    @staticmethod
+    async def on_pre_process_update(update: TelegramObject, data: dict):
+        logger.debug(update)
+
