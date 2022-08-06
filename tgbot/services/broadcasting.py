6c0c924bf5b98f18ef758ccf7408d6d212bb8c9e
Tim <herew26@gmail.com>
2022-08-06 21:57:04 +0300

    feat: add broadcasting to admins

---
+++ tgbot/services/broadcasting.py
@@ -0,0 +1,9 @@
+from aiogram_broadcaster import TextBroadcaster
+from aiogram import Bot
+
+from tgbot.config import Config
+
+
+async def send_to_admins(bot: Bot, text: str):
+    config: Config = bot.get('config')
+    await TextBroadcaster(config.tg_bot.admin_ids, text=text, bot=bot).run()
