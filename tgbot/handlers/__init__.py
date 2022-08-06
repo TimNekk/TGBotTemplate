951572da8d23e19e83f45375b8faebe5c65d9662
Tim <herew26@gmail.com>
2022-08-06 22:03:58 +0300

    feat: add __init__ for handlers

---
+++ tgbot/handlers/__init__.py
@@ -0,0 +1,9 @@
+from aiogram import Dispatcher
+
+from .admin import register_admin_handlers
+from .start import register_start_handlers
+
+
+def register_handlers(dp: Dispatcher):
+    register_admin_handlers(dp)
+    register_start_handlers(dp)
