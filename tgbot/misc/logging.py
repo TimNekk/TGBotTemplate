50fdffc7043f24b490a6b032b7981e3e996f18fe
Tim <herew26@gmail.com>
2022-08-09 01:26:41 +0300

    chore: set stderr logger level to info

--- tgbot/misc/logging.py
+++ tgbot/misc/logging.py
@@ -1,5 +1,9 @@
+import sys
+
 from loguru import logger
 
 
 def setup(file_name: str = "log"):
+    logger.remove()
+    logger.add(sys.stderr, level="INFO")
     logger.add(f"logs/{file_name}.log", rotation="00:00", level="DEBUG")
